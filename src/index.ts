441956376fbea3fd7424f2e7987c8e8029a921c1
Tone Test <92502536+Tone-Test@users.noreply.github.com>
2023-07-20 10:23:37 -0800

    gooning is real

---
+++ src/index.ts
@@ -0,0 +1,138 @@
+// some of the code taken from aeongdesu & original emnity plugin by spinfal (most of the code is from spinfal)
+import { registerCommand } from "@vendetta/commands"
+import { logger } from "@vendetta";
+import { findByProps } from "@vendetta/metro"
+import Settings from "./settings";
+import { storage } from '@vendetta/plugin';
+
+const MessageActions = findByProps("sendMessage", "receiveMessage")
+const Channels = findByProps('getLastSelectedChannelId')
+const BotMessage = findByProps('createBotMessage')
+const Avatars = findByProps("BOT_AVATARS")
+
+function sendReply(channelID, content, embed) {
+    const channel = channelID ?? Channels?.getChannelId?.();
+    const msg = BotMessage.createBotMessage({ channelId: channel, content: '', embeds: embed });
+    msg.author.username = 'Gooner Bro';
+    msg.author.avatar = 'Gooner Bro';
+    Avatars.BOT_AVATARS.Astolfo = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9eUtSXO_ZJnZR_9hai47aYQQiyy5rngsUqg&usqp=CAU';
+
+    if (typeof content === 'string') {
+        msg.content = content;
+    } else {
+        Object.assign(msg, content);
+    }
+
+    MessageActions.receiveMessage(channel, msg);
+}
+
+
+let commands = []
+
+commands.push(registerCommand({
+    name: "goon",
+    displayName: "goon",
+    description: "Get an image of a gooner",
+    displayDescription: "Get an image of a gooner",
+    options: [{
+        name: "nsfw",
+        displayName: "nsfw",
+        description: "idk",
+        displayDescription: "idk",
+        required: false,
+        type: 5
+    }, {
+        name: "sort",
+        displayName: "sort",
+        description: "Changes the way reddit sorts.",
+        displayDescription: "Changes the way reddit sorts",
+        required: false,
+        type: 3
+    }, {
+        name: "silent",
+        displayName: "silent",
+        description: "Makes it so only you can see the message.",
+        displayDescription: "Makes it so only you can see the message.",
+        required: false,
+        type: 5
+    }],
+    applicationId: "-1",
+    inputType: 1,
+    type: 1,
+    execute: async (args, ctx) => {
+        try {
+            //some code taken from emnity gotfemboys plugin by spinfal & was modified with the help of meqativ 
+            let nsfw = args.find(arg => arg.name === "nsfw")?.value
+            let sort = args.find(arg => arg.name === "sort")?.value
+            let silent = args.find(arg => arg.name === "silent")?.value
+            sendReply(ctx.channel.id, storage.ihateredditwarning, [])
+            if (storage.ihateredditwarning) {
+                if (typeof sort === "undefined") sort = storage.sortdefs;
+                if (!["best", "hot", "new", "rising", "top", "controversial"].includes(sort)) {
+                    sendReply(ctx.channel.id, "Incorrect sorting type. Valid options are\n`best`, `hot`, `new`, `rising`, `top`, `controversial`.", [])
+                    return
+                }
+                let response = await fetch(`https://www.reddit.com/r/GoonCaves/${sort}.json?limit=100`).then(res => res.json());
+                if (!ctx.channel.nsfw_ && nsfw && storage.nsfwwarn && !(silent ?? true)) {
+                    sendReply(ctx.channel.id, "This channel is not marked as NSFW\n(You can disable this check in plugin settings)", [])
+                    return
+                }
+                if (nsfw) { response = await fetch(`https://www.reddit.com/r/GoonCaves/${sort}.json?limit=100`).then(res => res.json()); }
+                response = response.data?.children?.[Math.floor(Math.random() * response.data?.children?.length)]?.data;
+                let author = await fetch(`https://www.reddit.com/u/${response?.author}/about.json`).then(res => res.json());
+
+                if (silent ?? true) {
+                    sendReply(ctx.channel.id, "", [{
+                        type: "rich",
+                        title: response?.title,
+                        url: `https://reddit.com${response?.permalink}`,
+                        author: {
+                            name: `u/${response?.author} • r/${response?.subreddit}`,
+                            proxy_icon_url: author?.data.icon_img.split('?')[0],
+                            icon_url: author?.data.icon_img.split('?')[0]
+                        },
+                        image: {
+                            proxy_url: response?.url_overridden_by_dest.replace(/.gifv$/g, ".gif") ?? response?.url.replace(/.gifv$/g, ".gif"),
+                            url: response?.url_overridden_by_dest?.replace(/.gifv$/g, ".gif") ?? response?.url?.replace(/.gifv$/g, ".gif"),
+                            width: response?.preview?.images?.[0]?.source?.width,
+                            height: response?.preview?.images?.[0]?.source?.height
+                        },
+                        color: "0xf4b8e4"
+                    }])
+                } else {
+                    MessageActions.sendMessage(ctx.channel.id, {
+                        content: response?.url_overridden_by_dest ?? response?.url
+                    })
+                }
+            } else {
+                storage.ihateredditwarning = true
+                sendReply(ctx.channel.id, "", [{
+                    type: "rich",
+                    title: "the saddest moment ever :(",
+                    description: "after the reddit api changes this plugin will not work anymore, thanks for using this joke plugin :3",
+                    footer: {
+                        "text": "this warning only happens once, you wont see this again."
+                    },
+                    color: "0xf4b8e4"
+                }])
+
+            }
+
+        } catch (err) {
+            logger.log(err);
+            sendReply(ctx.channel.id, "ERROR !!!!!!!!!!!! 😭😭😭 Check debug logs!! 🥺🥺🥺", [])
+        }
+    }
+}))
+
+export const settings = Settings;
+
+export const onLoad = () => {
+    storage.nsfwwarn ??= true
+    storage.sortdefs ??= "new"
+    storage.ihateredditwarning ??= false
+}
+
+export const onUnload = () => {
+    for (const unregisterCommands of commands) unregisterCommands()
+}
